8f93500f867efe8a4ad0d79475cbb27f3683134a
Tobias Bergkvist <tobias@bergkv.ist>
2019-12-11 10:03:04 +0100

    Add production mode to client.

--- services/client/fuse.prod.js
+++ services/client/fuse.prod.js
@@ -5,7 +5,5 @@ fusebox({
 	watch: true,
 	entry: 'src/index.ts',
 	target: 'browser',
-	devServer: { httpServer: { port: 8080 }, hmrServer: { port: 9999, enabled: true } },
 	webIndex: { template: 'src/index.html' },
-	sourceMap: true
-}).runProd({ screwIE: false, target: 'ES5', uglify: true})
+}).runProd({ screwIE: false, target: 'ES5', uglify: true })
